aef738114f9a0c58aeef03d91e6dc5cb0053a6d0
Robert <rgs2@icomp.ufam.edu.br>
2020-12-30 16:56:07 -0400

    feat: Validando a data

--- src/routes/appointements.routes.ts
+++ src/routes/appointements.routes.ts
@@ -1,17 +1,34 @@
 import { Router } from 'express';
 import { v4 } from 'uuid';
+import { startOfHour, parseISO, isEqual } from "date-fns";
 
 const appointementsRouter = Router();
 
-const appointements = [];
+interface Appointement {
+  id: string,
+  provider: string,
+  date: Date,
+}
+
+const appointements: Appointement[] = [];
 
 appointementsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parseDate = startOfHour(parseISO(date));
+
+  const findAppointementInSameDate = appointements.find(appointement =>
+    isEqual(parseDate, appointement.date)
+  );
+
+  if(findAppointementInSameDate) {
+    return response.status(400).json({ message: "This appointement is already booked" });
+  }
+
   const appointement = {
     id: v4(),
     provider,
-    date
+    date: parseDate,
   }
 
   appointements.push(appointement);
